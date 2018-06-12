f0463c5b95cc055a5ef4a157ed9fd0eca4e233f1
Taleb Mosyri <taleb_n1@hotmail.com>
2018-06-12 14:16:19 +0200

    Add stylesheet, add functionalit

---
+++ nameIT/app.js
@@ -0,0 +1,21 @@
+﻿// declare a list (array) of images title
+ var pics = [
+    "Images/adler-2872995_1920.jpg",
+    "Images/lion-3040797_1920.jpg",
+    "Images/mammal-3218712_1920.jpg",
+    "Images/monkey-1321358_1920.jpg",
+    "Images/owl-3340957_1920.jpg",
+    "Images/puma-2903312_1920.jpg"
+];
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+btn.addEventListener("click", function () {
+    if (counter === 6) {
+        counter = 0;
+    }
+    img.src = pics[counter]
+    counter = counter + 1;
+
+});
